e1a453e452afe88686b8d20f5d665da414782769
Clucas0311 <clucas0311@gmail.com>
2022-02-12 11:55:50 -0500

    Created FeedbackItem App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import "./App.css";
 import Header from "./components/Header";
+import FeedbackItem from "./components/FeedbackItem";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Header />
       <div className="container">
         <h1>My App</h1>
+        <FeedbackItem />
       </div>
     </>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const Header = () => {
+const Header = ({text, bgColor, textColor}) => {
+    const headerStyles = {
+        backgroundColor: bgColor,
+        color: textColor
+    }
   return (
-    <header>
+    <header style={headerStyles}>
         <div className="container">
-            <h2>Feedback UI</h2>
+            <h2>{text}</h2>
         </div>
     </header>
   )
+}
+// We set default props for a particular component if no prop is passed in
+Header.defaultProps = {
+    text: "Feedback UI",
+    bgColor: "rgba(0,0,0, 0.4)",
+    textColor: "#ff6a95"
+}
+
+// We can specify what type of prop ie (bool, string or array)
+Header.propTypes = {
+    text: PropTypes.string,
 }
 
 export default Header
